c959e1007bc4aae90bf3a187e4ef3b650d06d7b1
Nikhesh <nikhesh.p@kambala.co.in>
2023-01-23 12:07:13 +0530

    get_time_price_series method added

--- src/main/java/com/prostocks/starapi/java/StarApiJava.java
+++ src/main/java/com/prostocks/starapi/java/StarApiJava.java
@@ -7,6 +7,10 @@ package com.prostocks.starapi.java;
 import com.noren.javaapi.NorenApiJava;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.ZoneOffset;
 
 /**
  *
@@ -35,6 +39,26 @@ public class StarApiJava {
         if(book != null)
             System.out.println(book.toString());
         
-        
+         JSONArray ret1;
+        String ret = api.login("FA30417", "Daiwik@7", "062869", "FA30417_U", "afb8bd097e59100b74dae729eb4386de", "java-");
+        if (ret != null) {
+            
+            LocalDateTime lastBusDay = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
+            if (lastBusDay.getDayOfWeek().getValue() == 6) {
+                lastBusDay = lastBusDay.minusDays(1);
+            } else if (lastBusDay.getDayOfWeek().getValue() == 7) {
+                lastBusDay = lastBusDay.minusDays(2);
+            }
+        System.out.println(lastBusDay.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
+            long starttime = System.currentTimeMillis();
+            System.out.println("The start time is :" + starttime);
+             lastBusDay = lastBusDay.minusDays(2);
+     
+           ret1  = api.get_time_price_series("NSE", "10794", Long.toString(lastBusDay.toEpochSecond(ZoneOffset.UTC)), "1674000000", null);
+           System.out.println("The time difference is :" + (System.currentTimeMillis()-starttime) );
+            if(ret1 != null)
+           System.out.println(ret1.toString());
+
+            }    
     }
 }
